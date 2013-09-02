1d9ceea71f47b361f6cb14366e45bdc311c886dd
kaeza <kaeza@users.sf.net>
2013-09-02 14:48:39 -0300

    Small fixes.

--- lelbot.lua
+++ lelbot.lua
@@ -3,7 +3,7 @@
 package.path = "./?/init.lua;./irc/?.lua;./?.lua;"..package.path
 package.cpath = "./lib?.so;./?.dll;"..package.cpath
 
-local irc = require("irc")
+local luairc = require("irc")
 
 local SERV = "irc.freenode.net"
 local CHAN = "##mt-irc-mod"
@@ -11,7 +11,7 @@ local NICK = "meh"
 
 local ignores = { }
 
-local conn = irc.new({
+local conn = luairc.new({
 	nick = NICK,
 	username = "Meh",
 	realname = "Meh",
@@ -32,24 +32,37 @@ io = nil
 require = nil
 package = nil
 
+dofile = nil
+loadfile = nil
+load = nil
+module = nil
+socket = nil
+
+irc = nil
+
 function exit()
 	conn:disconnect("Bye!")
 	os_exit()
 end
 
+local out_text
+
 function print(...)
 	local s = table.concat({...}, " ")
 	s = s:gsub("[\001-\031]", " ")
-	conn:sendChat(CHAN, s)
+	table.insert(out_text, s)
 end
 
 conn:hook("OnChat", function(user, channel, message)
 	base_print(("[%s] %s: %s"):format(channel, user.nick, message))
 	if message:sub(1, 1) == "%" then
 		local f = loadstring(message:sub(2))
+		out_text = { }
 		local r, e = pcall(f)
-		if not r then
-			print("Error: "..tostring(e))
+		if r then
+			conn:sendChat(CHAN, user.nick..": "..table.concat(out_text, " "))
+		else
+			conn:sendChat(CHAN, user.nick..": Error: "..tostring(e))
 		end
 		return
 	end
